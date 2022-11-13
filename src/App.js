d37e6bbfbe0ec1919c1ad6a3f2172daa7ae4bafd
matigobbi <matigobbi@gmail.com>
2022-11-13 11:38:34 +0100

    land page added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import { useEffect } from 'react';
-
+import { useState } from 'react';
+import allTeamsData from "./allTeams.json"
 
 function App() {
-  useEffect(() => {
-  const headers = { 'Content-Type': 'application/json' }
-  fetch('https://api.npms.io/v2/search?q=react', { headers })
-      .then(response => response.json())
-      .then(data => this.setState({ totalReactPackages: data.total }));
-    }, []);
-  return (
-<div>hello there </div>
+
+  const [data, setData] = useState('');
+  const [allTeams , setAllTeams] = useState(allTeamsData)
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:5005/match`)
+  //     .then((res) => {return res.json()})
+  //    .then((response) => setData(response.data));
+  //  }, []);
+   console.log(allTeams)
+  return (<>
+    <h1>World cup 2022 Participants</h1>
+      <div id='teamsContainer'> 
+        {allTeams.map((one)=> (
+      
+        <p style={{"fontSize":"13px"}}>{one.name_en}
+        <br />
+        <img class="flag"src={one.flag} alt="" />
+        </p>
+      
+    )
+    )}
+      </div>
+    </>
   );
 }
 
